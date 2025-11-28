d604f262e83270b24bd23e7f347ea4b38122cb1d
Johny-1992 <120253850+Johny-1992@users.noreply.github.com>
2025-11-28 10:19:35 +0200

    Add configuration for fuel prices and types

---
+++ config.py
@@ -0,0 +1,15 @@
+# Prix des carburants (modifiable facilement)
+PRICES = {
+    "essence": 3830,
+    "mazout": 3960
+}
+
+# Liste des becs avec leur type de carburant
+BECS = {
+    "1a": "essence",
+    "1b": "mazout",
+    "2a": "essence",
+    "2b": "mazout",
+    "3a": "essence",
+    "3b": "mazout"
+}
--- config_prices.py
+++
@@ -1,3 +0,0 @@
-# Prix modifiables
-PRIX_ESSENCE = 3830   # FC
-PRIX_MAZOUT  = 3960   # FC
